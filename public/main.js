c9f899f021da54df568cc0992ff46c9815b0eefd
jeffreyc <jeffrey_chen@whirlpool.com>
2017-08-10 17:25:15 -0400

    added more examples

--- public/main.js
+++ public/main.js
@@ -2,7 +2,11 @@
 //listener is added
 window.onload = function() {
 	let el = document.querySelector('a-entity');
-	el.setAttribute('log', {event: 'anEvent', message: 'hello, metaverse'});
-	el.emit('anEvent');
-	console.log('event emitted');
+    el.setAttribute('log__helloworld', {event: 'worldEvent', message: 'hello, world'});
+    el.setAttribute('log__hellometaverse', {event: 'metaverseEvent', message: 'hello, metaverse'});
+    el.emit('worldEvent');
+    el.emit('metaverseEvent');
+
+    let follower = document.querySelector('#follower');
+    follower.setAttribute('follow', {target: '#leader', speed: 1});
 }
--- public/setup.js
+++ public/setup.js
@@ -12,6 +12,7 @@ AFRAME.registerComponent('log', {
 		message: {type: 'string', default: 'hello, world'},
 		event: {type: 'string', default: ''},
 	},
+    multiple: true,
 	init: function() {
 		this.eventHandler = () => {
 			console.log(this.data.message);
@@ -27,10 +28,74 @@ AFRAME.registerComponent('log', {
 
 		if(data.event) {
 			el.addEventListener(data.event, this.eventHandler);
-			console.log('event listener added');
 		}
 		else { //no event specified, just log message once
 			console.log(data.message);
 		}
-	}
+	},
+    remove: function() {
+        if(this.data.event) {
+            this.el.removeEventListener(this.data.event, this.eventHandler);
+        }
+    }
+});
+
+AFRAME.registerComponent('box', {
+    schema: {
+        width: {type: 'number', default: 1},
+        height: {type: 'number', default: 1},
+        depth: {type: 'number', default: 1},
+        color: {type: 'color', default: '#AAA'}
+    },
+    init: function() {
+        this.geometry = new THREE.BoxBufferGeometry(this.data.width, this.data.height, this.data.depth);
+        this.material = new THREE.MeshStandardMaterial({color: this.data.color});
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+        this.el.setObject3D('mesh', this.mesh);
+    },
+    update: function(oldData) {
+        if(Object.keys(oldData).length === 0) return;
+
+        //update the geometry
+        if(this.data.width !== oldData.width || this.data.height !== oldData.height || this.data.depth !== oldData.depth) {
+            this.el.getObject3D('mesh').geometry = new THREE.BoxBufferGeometry(this.data.width, this.data.height, this.data.depth);
+        }
+        //update the material
+        if(this.data.color !== oldData.color) {
+            this.el.getObject3D('mesh').material.color = this.data.color;
+        }
+    },
+    remove: function() {
+        this.el.removeObject3D('mesh');
+    }
+});
+
+AFRAME.registerComponent('follow', {
+    schema: {
+        target: {type: 'selector'},
+        speed: {type: 'number'}
+    },
+    init: function() {
+        this.directionVec3 = new THREE.Vector3();
+    },
+    tick: function(time, delta) {
+        let targetPosition = this.data.target.object3D.position; //position of the target
+        let currentPosition = this.el.object3D.position; //position of this element. it follows the target
+        let directionVec3 = this.directionVec3;
+        directionVec3.copy(targetPosition).sub(currentPosition);
+        let distance = directionVec3.length();
+        if(distance < 1) return;
+
+        let factor = this.data.speed / distance;
+        ['x', 'y', 'z'].forEach(function(axis) {
+            directionVec3[axis] *= factor * (delta / 1000);
+        });
+
+        this.el.setAttribute('position', {
+            x: currentPosition.x + directionVec3.x,
+            y: currentPosition.y + directionVec3.y,
+            z: currentPosition.z + directionVec3.z
+        });;
+    }
 });
